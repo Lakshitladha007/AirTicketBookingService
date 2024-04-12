31da56c43b36974a27fc638b0757668b5c528dea
Lakshit Ladha <lakshitladha05@gmail.com>
2024-04-13 01:50:58 +0530

    Added error files

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,19 @@
+// This is a generic error file
+
+class AppError extends Error{
+
+    constructor(
+        name,
+        message,
+        explaination,
+        statusCode
+    ){
+        super();
+        this.name= name;
+        this.message= message;
+        this.explaination= explaination;
+        this.statusCode= statusCode;
+    }
+}
+
+module.exports= AppError;
